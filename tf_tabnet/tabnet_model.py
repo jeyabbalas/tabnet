e19d63d6773d7158108e9d9315420a85f08ebb55
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2021-11-08 10:20:18 -0500

    Basic encoder flow

--- tf_tabnet/tabnet_model.py
+++ tf_tabnet/tabnet_model.py
@@ -49,7 +49,7 @@ class GLULayer(tf.keras.layers.Layer):
 
     def build(
         self, 
-        input_shape: tf.TensorShape
+        input_shape: tf.TensorShape, 
     ):
         if self.groups: # Group Normalization
             if self.groups == -1:
@@ -240,7 +240,7 @@ class Split(tf.keras.layers.Layer):
 class AttentiveTransformer(tf.keras.layers.Layer):
     def __init__(
         self, 
-        units: int = 8, 
+        units: int, 
         virtual_batch_size: Optional[int] = None, 
         momentum: float = 0.98, 
         groups: Optional[int] = None, 
@@ -254,11 +254,8 @@ class AttentiveTransformer(tf.keras.layers.Layer):
         Parameters:
         -----------
         units: int
-            Number of units in layer. Default (8).
-        mask_type: str
-            mask_type ∈ {"softmax", "entmax", "sparsemax"}. Softmax generates a dense mask.
-            Entmax (i.e. entmax 1.5) generates a slightly sparser mask. Sparsemax generates 
-            a highly sparse mask. To learn more, refer: https://arxiv.org/abs/1905.05702.
+            Number of units in layer. This layer outputs a mask for your data, so the 
+            number of units should be the same as your data dimension.
         virtual_batch_size: int
             Batch size for Ghost Batch Normalization (GBN). Value should be much smaller 
             than and a factor of the overall batch size. Default (None) runs regular batch 
@@ -274,6 +271,11 @@ class AttentiveTransformer(tf.keras.layers.Layer):
             virtual_batch_size. If an integer value is specified, Group Normalization is 
             run with that number of groups. For Layer Normalization, set group=1. For 
             Instance Normalization, set group=-1.
+        mask_type: str
+            mask_type ∈ {"softmax", "entmax", "sparsemax"}. Softmax generates a dense mask.
+            Entmax (i.e. entmax 1.5) generates a slightly sparser mask. Default(sparsemax) 
+            generates a highly sparse mask. 
+            To learn more, refer: https://arxiv.org/abs/1905.05702.
         """
         super(AttentiveTransformer, self).__init__(**kwargs)
         self.virtual_batch_size = virtual_batch_size
@@ -295,7 +297,7 @@ class AttentiveTransformer(tf.keras.layers.Layer):
         
     def build(
         self, 
-        input_shape: tf.TensorShape
+        input_shape: tf.TensorShape, 
     ):
         if self.groups: # Group Normalization
             if self.groups == -1:
@@ -308,14 +310,28 @@ class AttentiveTransformer(tf.keras.layers.Layer):
     def call(
         self, 
         inputs: Union[tf.Tensor, np.ndarray], 
-        priors: Optional[Union[tf.Tensor, np.ndarray]], 
+        prior: Optional[Union[tf.Tensor, np.ndarray]], 
         training: Optional[bool] = None, 
-    ):
+    ) -> tf.Tensor:
         x = self.fc(inputs)
         x = self.bn(x, training=training)
-        x = tf.multiply(priors, x)
+        x = tf.multiply(prior, x)
         x = self.sparse_activation(x)
         return x
+
+
+class AggregateFeatures(tf.keras.layers.Layer):
+    def __init__(
+        self, 
+        **kwargs
+    ):
+        super(AggregateFeatures, self).__init__(**kwargs)
+
+    def call(
+        self, 
+        inputs: Union[tf.Tensor, np.ndarray], 
+    ) -> tf.Tensor:
+        return tf.math.reduce_sum(inputs, axis=-1)
 
 
 class TabNetEncoder(tf.keras.layers.Layer):
@@ -333,6 +349,7 @@ class TabNetEncoder(tf.keras.layers.Layer):
         momentum: float = 0.98, 
         groups: Optional[int] = None, 
         mask_type: str = "sparsemax", 
+        lambda_sparse: float = 1e-3, 
         **kwargs
     ):
         """
@@ -385,62 +402,110 @@ class TabNetEncoder(tf.keras.layers.Layer):
             Instance Normalization, set group=-1.
         mask_type: str
             mask_type ∈ {"softmax", "entmax", "sparsemax"}. Softmax generates a dense mask.
-            Entmax (i.e. entmax 1.5) generates a slightly sparser mask. Sparsemax generates 
-            a highly sparse mask. To learn more, refer: https://arxiv.org/abs/1905.05702.
+            Entmax (i.e. entmax 1.5) generates a slightly sparser mask. Default(sparsemax) 
+            generates a highly sparse mask. 
+            To learn more, refer: https://arxiv.org/abs/1905.05702.
+        lambda_sparse: float
+            Coefficient for the mask sparsity loss. Important parameter to tune. Lower values 
+            lead to better performance. Default (1e-3).
         """
         super(TabNetEncoder, self).__init__(**kwargs)
+        self.n_steps = n_steps
+        self.n_dependent_glus = n_dependent_glus
+        self.decision_dim = decision_dim
+        self.attention_dim = attention_dim
+        self.virtual_batch_size = virtual_batch_size
+        self.momentum = momentum
+        self.groups = groups
+        self.mask_type = mask_type
 
         # plain batch normalization
-        self.initial_bn = tf.keras.layers.BatchNormalization(momentum=momentum)
+        self.initial_bn = tf.keras.layers.BatchNormalization(momentum=self.momentum)
 
         # shared glu block
         self.shared_glu_block = None
         if n_shared_glus > 0:
             self.shared_glu_block = GLUBlock(
                 n_glu_layers=n_shared_glus, 
-                units=decision_dim+attention_dim, 
-                virtual_batch_size=virtual_batch_size, 
-                momentum=momentum, 
-                groups=groups, 
+                units=self.decision_dim+self.attention_dim, 
+                virtual_batch_size=self.virtual_batch_size, 
+                momentum=self.momentum, 
+                groups=self.groups, 
             )
         
         # initial feature transformer
         self.initial_feature_transformer = FeatureTransformer(
-            n_dependent_glus=n_dependent_glus, 
+            n_dependent_glus=self.n_dependent_glus, 
             shared_layers=self.shared_glu_block, 
-            units=decision_dim+attention_dim, 
-            virtual_batch_size=virtual_batch_size, 
-            momentum=momentum, 
-            groups=groups, 
+            units=self.decision_dim+self.attention_dim, 
+            virtual_batch_size=self.virtual_batch_size, 
+            momentum=self.momentum, 
+            groups=self.groups, 
+            name="FeatureTransformer_Step_0", 
         )
 
         # split layer
         self.split_layer = Split(split_dim=decision_dim)
 
-        # feature and attentive transformers per step
+        # aggregate layer
+    
+    def build(
+        self, 
+        input_shape: tf.TensorShape, 
+    ):
+        input_dims = input_shape[-1]
+
+        # feature and attentive transformers for each step
         self.step_feature_transformers = list()
         self.step_attentive_transformers = list()
-        for step in range(n_steps):
+        for step in range(self.n_steps):
             feature_transformer = FeatureTransformer(
-                n_dependent_glus=n_dependent_glus, 
+                n_dependent_glus=self.n_dependent_glus, 
                 shared_layers=self.shared_glu_block, 
-                units=decision_dim+attention_dim, 
-                virtual_batch_size=virtual_batch_size, 
-                momentum=momentum, 
-                groups=groups, 
+                units=self.decision_dim+self.attention_dim, 
+                virtual_batch_size=self.virtual_batch_size, 
+                momentum=self.momentum, 
+                groups=self.groups, 
+                name=f"FeatureTransformer_Step_{(step+1)}", 
+            )
+            attentive_transformer = AttentiveTransformer(
+                units=input_dims, 
+                virtual_batch_size=self.virtual_batch_size, 
+                momentum=self.momentum, 
+                groups=self.groups, 
+                mask_type = self.mask_type, 
+                name=f"AttentiveTransformer_Step_{(step+1)}", 
             )
             self.step_feature_transformers.append(
                 feature_transformer
+            )
+            self.step_attentive_transformers.append(
+                attentive_transformer
             )
 
     def call(
         self, 
         inputs: Union[tf.Tensor, np.ndarray], 
+        prior: Union[tf.Tensor, np.ndarray] = None, 
         training: Optional[bool] = None, 
     ):
-        feat = self.initial_bn(inputs, training=training)
-        x = self.initial_feature_transformer(feat, training=training)
-        x_d, x_a = self.split_layer(x)
+        if not prior:
+            prior = tf.ones(tf.shape(inputs))
+        
+        x = self.initial_bn(inputs, training=training)
+        x_proc = self.initial_feature_transformer(x, training=training)
+        _, x_a = self.split_layer(x_proc)
+
+        for step in range(self.n_steps):
+            mask = self.step_attentive_transformers[step](x_a, 
+                                                          prior=prior, 
+                                                          training=training)
+            masked_x = tf.multiply(mask, x)
+            x_proc = self.step_feature_transformers[step](masked_x, 
+                                                          training=training)
+            x_d, x_a = self.split_layer(x_proc)
+            step_out = tf.keras.activations.relu(x_d)
+
 
 
 class TabNet(tf.keras.layers.Layer):
